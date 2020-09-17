aed91791e0178fd454ff592b80be741cf13ee3b2
sou <sasaki.sou@samurai-adways.net>
2020-09-17 21:23:43 +0900

    main修正

--- building_ad/main.rb
+++ building_ad/main.rb
@@ -1,7 +1,7 @@
 require_relative 'models'
 
 def company_post_ad_to_building(company_name, building_name, ad_image)
-  ad = Ad.new(owner: company_name, image: 'someimage')
+  ad = Ad.new(owner: company_name, image: ad_image)
   company = Company.new(name: company_name)
   building = Building.new(name: building_name)
   company.request_ad(company, ad)
